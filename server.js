6eae94a9271fc6bb3dd3db8e7e335d7508eeda59
Cray J Thomas Jr <cjthoma1@gmail.com>
2017-06-03 07:16:43 +0000

    Put parsed results back

--- server.js
+++ server.js
@@ -8,13 +8,11 @@ port = process.env.PORT || '8080',
 routes = require("./routes/api"),
 mongo = require("mongodb"),
 monk = require("monk"),
-//db = monk(process.env.MONGOLAB_URI),
-db = monk('localhost:27017/images'),
+db = monk(process.env.MONGOLAB_URI),
+//db = monk('localhost:27017/images'),
 
 imageSearch = require('node-google-image-search'),
 GoogleImages = require('google-images');
-
-
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
